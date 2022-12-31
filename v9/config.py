05bf653077f57f8bff818b2d504635786f0e076a
Prajwal Singh <prajwalrishabh@gmail.com>
2022-12-31 19:03:49 +0530

    Add files via upload

---
+++ v9/config.py
@@ -0,0 +1,32 @@
+import os
+import math
+
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
+os.environ["CUDA_DEVICE_ORDER"]= "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]= '1'
+
+train_json_path = 'dataset/nerf_synthetic/ficus/transforms_train.json' 
+val_json_path   = 'dataset/nerf_synthetic/ficus/transforms_val.json' 
+image_path      = 'dataset/nerf_synthetic/ficus'
+device       = 'cuda'
+lr           = 5e-4
+image_height = 512
+image_width  = 512
+batch_size   = 1
+epochs       = 10000
+vis_freq     = 10
+
+pos_enc_dim  = 10
+dir_enc_dim  = 4
+
+near_plane   = 2.0
+far_plane    = 6.0
+num_samples  = 64
+net_dim      = 256
+in_feat      = 63
+dir_feat     = 27
+# in_feat      = 60
+skip_layer   = 4
+net_depth    = 8
+num_samples_fine  = 2 * num_samples
+n_samples    = 1024
---
+++ v9/utils.py
@@ -0,0 +1,31 @@
+import os
+import matplotlib.pyplot as plt
+from matplotlib import style
+from torchvision import transforms
+import numpy as np
+
+plt.rcParams["savefig.bbox"] = 'tight'
+style.use('seaborn')
+# https://pytorch.org/vision/stable/auto_examples/plot_visualization_utils.html
+def show(imgs, path, label, idx):
+    path = '{}/{}/'.format(path, label)
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    if not isinstance(imgs, list):
+        imgs = imgs[:]
+
+    fig, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = transforms.functional.to_pil_image(img)
+        img = np.asarray(img)
+        if len(img.shape) < 3:
+            axs[0, i].imshow(img, cmap='viridis')
+        else:
+            axs[0, i].imshow(img)
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+    # plt.show()
+    # plt.close()
+    plt.savefig('{}/{}.png'.format(path, idx))
+    plt.close()
