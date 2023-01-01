b82e715911077c08b9ea8c50df39d46f8ebfa382
Prajwal Singh <prajwalrishabh@gmail.com>
2023-01-01 08:27:12 +0530

    Add files via upload

--- v9/render_view_new.py
+++ v9/render_view_new.py
@@ -67,10 +67,10 @@ if __name__ == '__main__':
 	focal = torch.as_tensor([177.77], dtype=torch.float32)
 	rgb_frames = []
 
-	nerfnet_coarse = NerfNet(depth=config.net_depth, in_feat=config.in_feat,\
+	nerfnet_coarse = NerfNet(depth=config.net_depth, in_feat=config.in_feat, dir_feat=config.dir_feat,\
 					  net_dim=config.net_dim, skip_layer=config.skip_layer).to(config.device)
 
-	nerfnet_fine   = NerfNet(depth=config.net_depth, in_feat=config.in_feat,\
+	nerfnet_fine   = NerfNet(depth=config.net_depth, in_feat=config.in_feat, dir_feat=config.dir_feat,\
 					  net_dim=config.net_dim, skip_layer=config.skip_layer).to(config.device)
 
 	nerfnet_coarse = torch.nn.DataParallel(nerfnet_coarse).to(config.device)
@@ -98,8 +98,8 @@ if __name__ == '__main__':
 	# 	os.makedirs('EXPERIMENT_{}'.format(experiment_num))
 
 	# os.system('cp *.py EXPERIMENT_{}'.format(experiment_num))
-	label = 'lego'
-	experiment_num = 116
+	label = 'ficus'
+	experiment_num = 2
 	ckpt_path  = natsorted(glob('EXPERIMENT_{}/checkpoints/nerf_*.pth'.format(experiment_num)))[-1]
 
 	if os.path.isfile(ckpt_path):		
@@ -126,24 +126,28 @@ if __name__ == '__main__':
 
 			ray_origin, ray_direction = nerf_comp.get_rays(c2w)
 			ray_origin_o, ray_direction_o = ray_origin.reshape(config.batch_size, -1, 3),\
-									ray_direction.reshape(config.batch_size, -1, 3)											
+									ray_direction.reshape(config.batch_size, -1, 3)
 
 			for idx  in range(0, config.image_height*config.image_width, config.n_samples):
 
 				ray_origin, ray_direction = ray_origin_o[:, idx:idx+config.n_samples], ray_direction_o[:, idx:idx+config.n_samples]
+				ray_direction_c  = torch.tile(torch.unsqueeze(torch.squeeze(ray_direction, dim=0), dim=-2), (1, config.num_samples, 1))
+				ray_direction_c  = nerf_comp.encode_position(x=ray_direction_c, enc_dim=config.dir_enc_dim)
+				ray_direction_f  = torch.tile(torch.unsqueeze(torch.squeeze(ray_direction, dim=0), dim=-2), (1, config.num_samples_fine, 1))
+				ray_direction_f  = nerf_comp.encode_position(x=ray_direction_f, enc_dim=config.dir_enc_dim)	
 				# ray_origin, ray_direction = nerf_comp.ndc_rays(ray_origin, ray_direction)
 
 				rays, t_vals     = nerf_comp.sampling_rays(ray_origin=ray_origin, ray_direction=ray_direction, random_sampling=True)
 
-				prediction   = nerfnet_coarse(rays)
+				rgb, density   = nerfnet_coarse(rays, ray_direction_c)
 
-				rgb_coarse, depth_map_coarse, weights_coarse = nerf_comp.render_rgb_depth(prediction=prediction, rays=rays, t_vals=t_vals, random_sampling=True)
+				rgb_coarse, depth_map_coarse, weights_coarse = nerf_comp.render_rgb_depth(rgb=rgb, density=density, rays=rays, t_vals=t_vals, random_sampling=True)
 
 				fine_rays, t_vals_fine = nerf_comp.sampling_fine_rays(ray_origin=ray_origin, ray_direction=ray_direction, t_vals=t_vals, weights=weights_coarse)
 
-				prediction   = nerfnet_fine(fine_rays)
+				rgb, density   = nerfnet_fine(fine_rays, ray_direction_f)
 
-				rgb_fine, depth_map_fine, weights_fine = nerf_comp.render_rgb_depth(prediction=prediction, rays=fine_rays, t_vals=t_vals_fine, random_sampling=True)
+				rgb_fine, depth_map_fine, weights_fine = nerf_comp.render_rgb_depth(rgb=rgb, density=density, rays=fine_rays, t_vals=t_vals_fine, random_sampling=True)
 
 				rgb_final.append(rgb_fine)
 				depth_final.append(depth_map_fine)
@@ -155,7 +159,7 @@ if __name__ == '__main__':
 			# rgb = torch.permute(rgb, (0, 3, 1, 2))
 			# show(imgs=rgb[:1], path='EXPERIMENT_{}/train'.format(experiment_num), label='img', idx=epoch)
 			# show(imgs=depth_map[:1], path='EXPERIMENT_{}/train'.format(experiment_num), label='depth', idx=epoch)
-			rgb_frames = rgb_frames + [ np.uint8(np.clip(rgb_final[0].detach().cpu().numpy()*255.0, 0, 255)) ]
+			rgb_frames = rgb_frames + [ np.uint8(np.clip(rgb_final.detach().cpu().numpy()*255.0, 0, 255)) ]
 
 	rgb_video = "EXPERIMENT_{}/{}.mp4".format(experiment_num, label)
 	imageio.mimwrite(rgb_video, rgb_frames, fps=30, quality=7, macro_block_size=None)
